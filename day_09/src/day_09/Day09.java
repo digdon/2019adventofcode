38e635c1efbdc70cf01608b8edd120294e8fec28
Mike Digdon <Mike.Digdon@viacomcontractor.com>
2020-02-17 15:18:45 -0400

    Day 9 solution

---
+++ day_09/src/day_09/Day09.java
@@ -0,0 +1,48 @@
+package day_09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Day09 {
+
+    public static void main(String[] args) {
+        // Load the program from input
+        List<Long> codeArray = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine = null;
+
+        try {
+            while ((inputLine = reader.readLine()) != null) {
+                String[] split = inputLine.split("\\s*,\\s*");
+                
+                for (int i = 0; i < split.length; i++) {
+                    codeArray.add(new Long(split[i]));
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        try {
+            reader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        Long[] program = new Long[codeArray.size()];
+        program = codeArray.toArray(program);
+        
+        IntCodeComputer computer = new IntCodeComputer(program, new LinkedList<Long>(), null);
+        computer.inputValue(1);
+        System.out.println(computer.execute());
+        
+        computer.reset();
+        computer.inputValue(2);
+        System.out.println(computer.execute());
+    }
+}
