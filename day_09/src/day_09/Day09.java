26c13e521cc04969dd53c1ec697002cbd4fed54c
Mike Digdon <Mike.Digdon@viacomcontractor.com>
2020-03-08 21:34:45 -0300

    Solution for day 15
    Also modified IntComputer and refactored days 9 (including 7 redux), 11,
    and 13 to use the new version.

--- day_09/src/day_09/Day09.java
+++ day_09/src/day_09/Day09.java
@@ -1,44 +1,13 @@
 package day_09;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 import aoc2019.IntCodeComputer;
 
 public class Day09 {
 
     public static void main(String[] args) {
-        // Load the program from input
-        List<Long> codeArray = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String inputLine = null;
-
-        try {
-            while ((inputLine = reader.readLine()) != null) {
-                String[] split = inputLine.split("\\s*,\\s*");
-                
-                for (int i = 0; i < split.length; i++) {
-                    codeArray.add(new Long(split[i]));
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.exit(-1);
-        }
-        
-        try {
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-        
-        Long[] program = new Long[codeArray.size()];
-        program = codeArray.toArray(program);
+        Long[] program = IntCodeComputer.loadProgramFromInput(System.in);
         
         IntCodeComputer computer = new IntCodeComputer(program, new LinkedList<Long>(), null);
         computer.inputValue(1);
